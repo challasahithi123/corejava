520ab0b4ea70c08420a09a07e7dc96b3acb7a56c
LENOVO <LENOVO@DESKTOP-VE7NBKO>
2024-07-03 15:24:27 +0530

    commit mesage

---
+++ corejavaprogram/src/com/tnsif/packages/Publicdemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.packages;
+
+public class Publicdemo {
+int a=8;
+public static void main(String[] args){
+	Publicdemo p=new Publicdemo();
+System.out.println(p.a);
+}
+}
---
+++ corejavaprogram/src/com/tnsif/scanner/Scannerexample.java
@@ -0,0 +1,15 @@
+package com.tnsif.scanner;
+
+import java.util.Scanner;
+
+public class Scannerexample{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your name");
+		String name=sc.nextLine();
+		System.out.println("enter your roll number");
+		int roll=sc.nextInt();
+	}
+
+}
