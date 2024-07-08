c23e1513d2206a4ff1f83da733441253b0c9d8bb
LENOVO <LENOVO@DESKTOP-VE7NBKO>
2024-07-08 12:10:59 +0530

    commit message

---
+++ corejavaprogram/src/com/tnsif/introduction/Narrowingexample.java
@@ -0,0 +1,11 @@
+package com.tnsif.introduction;
+
+public class Narrowingexample {
+	public static void main(String[] args) {
+	double d=8.9;
+	int i=(int)d;
+	System.out.println("before modification"+d);
+	System.out.println("after modification"+i);
+}
+}
+
---
+++ corejavaprogram/src/com/tnsif/introduction/Wideningexample.java
@@ -0,0 +1,11 @@
+package com.tnsif.introduction;
+
+public class Wideningexample {
+	public static void main(String [] args)
+	{
+		int x=7;
+		float y=x;
+		System.out.println("before modification" + x);
+	    System.out.println("after modification" + y);
+	} 
+}
